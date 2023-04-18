e03b6a5115ffb192083d3dc7ef980c9462308a70
Wattima <darwattima@gmail.com>
2023-04-19 00:10:37 +0300

    payment and spaces controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,45 @@
+
 class PaymentsController < ApplicationController
+  before_action :set_payment, only: [:show, :update, :destroy]
+
+  def index
+    @payments = Payment.all
+    render json: @payments
+  end
+
+  def show
+    render json: @payment
+  end
+
+  def create
+    @payment = Payment.new(payment_params)
+
+    if @payment.save
+      render json: @payment, status: :created
+    else
+      render json: @payment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @payment.update(payment_params)
+      render json: @payment
+    else
+      render json: @payment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @payment.destroy
+  end
+
+  private
+
+  def set_payment
+    @payment = Payment.find(params[:id])
+  end
+
+  def payment_params
+    params.require(:payment).permit(:user_id, :space_id, :booking_id, :amount, :status)
+  end
 end
