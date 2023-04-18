2e76e1ffd23293fb06aa5f22ffe594880ecc560f
seansama <shaun.mwangi@student.moringaschool.com>
2023-04-18 10:45:01 +0300

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,38 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_151903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "bookings", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
     t.integer "space_id"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.decimal "amount", null: false
+    t.string "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spaces", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "location", null: false
+    t.decimal "hourly_rate", precision: 10, scale: 2, null: false
+    t.decimal "daily_rate", precision: 10, scale: 2, null: false
+    t.jsonb "available_dates", default: {}, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
