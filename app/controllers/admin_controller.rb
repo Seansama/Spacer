200a84cbf9eff098cc592775e5d25be260c33925
seansama <shaun.mwangi@student.moringaschool.com>
2023-04-19 10:22:46 +0300

    Created routes for admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,45 @@
 class AdminController < ApplicationController
+
+  def show
+    if (admin = Admin.find_by(id: params[:id]))
+      render json: admin, status: :ok
+    else
+      render json: {error: 'This admin does not exist'}, status: :not_found
+    end
+
+    def create
+      if (admin = Admin.create(admin_params))
+        render json: admin, status: :created
+      else
+        render json: {error: admin.errors.full_messages}, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def update
+    admin = Admin.find_by(id: params[:id])
+    if admin.update(admin_update)
+      render json: admin, status: :accepted
+    else
+      render json: {error: admin.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    admin = Admin.find_by(id: params[:id])
+    if admin.destroy
+      head :no_content
+    else
+      render json: {error: 'This user could not be deleted. Perhaps check if they exist?'}, status: :not_found
+    end
+  end
+
+  private
+  def admin_params
+    params.require(:admin).permit(:name, :email, :password)
+  end
+
+  def admin_update
+    params.require(:admin).permit(:email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users
-  resources :bookings, only: [:index, :show, :destroy, :edit, :update, :create, :new ] 
-
+  resources :bookings, only: [:index, :show, :destroy, :edit, :update, :create, :new ]
+  resources :admin, only: [:show, :create, :update, :destroy]
 end
