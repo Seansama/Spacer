c1ad501d91c8b3356c7ce6b3e275f53eb0528105
seansama <shaun.mwangi@student.moringaschool.com>
2023-04-23 12:32:54 +0300

    Fixed admin register action

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_22_201532) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_23_084635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
